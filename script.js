ce70c562ee563a8c6d7c6a6a6533b3a1900ee70c
Rebeca Santos <rcks.santos@gmail.com>
2020-06-13 08:48:41 -0300

    define movimento da cobrinha

--- script.js
+++ script.js
@@ -3,27 +3,53 @@ let canvas = document.getElementById('snake')
 let context = canvas.getContext('2d')
 let box = 32
 
-//criando a cobrinha - dando um tamanho inicial a ela
+//criando a cobrinha - dando características a ela
 let cobrinha=[]
 cobrinha[0] = {
   x: 8 * box,
   y: 8 * box
 }
+let direction = "right"
 
 
 //criando o BG
 function criarBg() {
-    context.fillStyle = 'lightgreen';
+    context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
 //criando a cobrinha - mecânica 
 function criarCobrinha() {
   for (i=0 ; i < cobrinha.length; i++) {
-    context.fillStyle = 'green'
+    context.fillStyle = "green"
     context.fillRect(cobrinha[i].x, cobrinha[i].y, box, box)
   }
 }
 
-criarBg()
-criarCobrinha()
+//iniciar o jogo 
+function iniciarJogo() {
+  criarBg()
+  criarCobrinha()
+
+  let cobrinhaX = cobrinha[0].x
+  let cobrinhaY = cobrinha[0].y
+  // cria o movimento da cobrinha 
+  if(direction == "right") cobrinhaX += box
+  if(direction == "left") cobrinhaX -= box
+  if(direction == "down") cobrinhaY += box
+  if(direction == "up") cobrinhaY -= box
+
+  //remove o último item do array
+  cobrinha.pop()
+
+  //acrescenta novo item à primeira posição do array
+  let novaCabeca = {
+    x: cobrinhaX,
+    y: cobrinhaY
+  }
+
+  cobrinha.unshift(novaCabeca)
+  
+}
+
+let jogo = setInterval(iniciarJogo, 100)
