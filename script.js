fe239773122f7ee2c1e67277246422ae1dc2bc78
Rebeca Santos <rcks.santos@gmail.com>
2020-06-13 08:24:43 -0300

    cria cobrinha padrão

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
+//elementos do palco, BG
 let canvas = document.getElementById('snake')
 let context = canvas.getContext('2d')
 let box = 32
 
+//criando a cobrinha - dando um tamanho inicial a ela
+let cobrinha=[]
+cobrinha[0] = {
+  x: 8 * box,
+  y: 8 * box
+}
+
+
+//criando o BG
 function criarBg() {
     context.fillStyle = 'lightgreen';
     context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
+//criando a cobrinha - mecânica 
+function criarCobrinha() {
+  for (i=0 ; i < cobrinha.length; i++) {
+    context.fillStyle = 'green'
+    context.fillRect(cobrinha[i].x, cobrinha[i].y, box, box)
+  }
+}
+
 criarBg()
+criarCobrinha()
